5e756e040c4241e8726877bfc5e8208f5f5ee813
Renata Poltorak <renpoltorak@gmail.com>
2025-02-26 16:24:03 +0100

    Ćwiczenie — tworzenie interfejsu użytkownika za pomocą składników platformy Blazor

--- Model/Pizza.cs
+++ Model/Pizza.cs
@@ -22,6 +22,16 @@ namespace BlazingPizza
 
         public int Size { get; set; }
 
+        public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public decimal Price { get; set; }
+    
+    public bool Vegetarian { get; set; }
+    
+    public bool Vegan { get; set; }
+
         public List<PizzaTopping> Toppings { get; set; }
 
         public decimal GetBasePrice()
